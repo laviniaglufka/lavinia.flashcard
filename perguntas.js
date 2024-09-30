5848d22e81ddb4a6747ce749cbcecbb0130d29e8
laviniaglufka <143838281+laviniaglufka@users.noreply.github.com>
2024-09-30 08:38:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Saúde e Bem Estar',
+    'O que são proteínas?',
+    'nutrientes que ajudam na construção e reparação de tecidos'
+)
+
+criaCartao(
+    'Saúde e Bem Estar',
+    'O que significa IMC?',
+    'Índice de Massa Corporal'
+)
+
+criaCartao(
+    'Saúde e Bem Estar',
+    'O que são carboidratos?',
+    'Fonte de energia para o corpo, encontrada em alimentos como pão, arroz e batata'
+)
+
+criaCartao(
+    'Saúde e Bem Estar',
+    'O que é hipertensão?',
+    'Pressão arterial elevada, que pode aumentar o risco de doenças cardíacas'
+)
